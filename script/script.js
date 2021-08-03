ee60e59f15df9f3e93dd1dab1d0df08701e97d8c
rdw5373 <rdw5373@naver.com>
2021-08-03 14:39:52 +0900

    0803_GNB구현완료

--- script/script.js
+++ script/script.js
@@ -1,5 +1,12 @@
-$(function () {
+$(function(){
+
+    // GNB 메뉴 blink(깜박임 효과) 시작
+    setInterval(fnBlink, 580);
+
+    function fnBlink(){
+        $("#blinkStr").toggle();
+    }
+    // GNB 메뉴 blink(깜박임 효과) 끝
 
 
-
-});
+});  // 에밋코드
